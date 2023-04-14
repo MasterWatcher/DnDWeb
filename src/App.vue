9423e5c57f5b755f3444085fa4eeee3944197d86
Anton Goncharov <anton.goncharov@mybuddy.ai>
2023-04-14 19:23:40 +0300

    Pusher test

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,39 @@
 
 <script>
-import AddTutorial from './components/AddTutorial.vue'
-
-export default {
-  components: {
-    AddTutorial
+  import Pusher from 'pusher-js'
+  export default {
+    data() {
+      return {
+        events: []
+      }
+    }, 
+    mounted() {
+      const pusher = new Pusher('c6e6ebf74db06fb56db2', {
+        cluster: 'eu'
+      })
+      const channel = pusher.subscribe('my-channel')
+      channel.bind('my-event', data => {
+        console.log('Received data:', data)
+        this.events.push(data)
+      })
+    }
   }
-}
 </script>
 
 <template>
-   <AddTutorial />
+  <div v-for="event in events" :key="event.id">
+    <h2>{{ event.title }}</h2>
+    <p>{{ event.description }}</p>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
